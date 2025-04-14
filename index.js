9dd70726a10019f1eec469eeb465764ab45fc07b
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-04-14 11:19:39 +0200

    server side

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors"
+
+const app = express();
+
+app.use(cors({ origin: ["http://localhost:5173"] }));
+
+const sampleEmployee = {
+    name: {
+        first: "Charlie",
+        last: "Thompson",
+    },
+    email: "charlie.thompson@example.com",
+    picture: {
+        medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+    },
+};
+
+const employeeList = {
+    results: [
+        sampleEmployee
+    ]
+}
+
+app.get("/api/employees", (req, res) => {
+    res.json({ results: employeeList.results });
+    //res.json({ results: [sampleEmployee] });
+});
+
+const port = 3310;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
