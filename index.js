cbdaeeb737727ddcd4d0850e5fd87fa1c1c7d420
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-04-17 13:42:03 +0200

    Update index.js
    solving issues #1 and #2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors"
 
 const app = express();
+const port = 3310;
 
 app.use(cors({ origin: ["http://localhost:5173"] }));
 
@@ -24,10 +25,7 @@ const employeeList = {
 
 app.get("/api/employees", (req, res) => {
     res.json({ results: employeeList.results });
-    //res.json({ results: [sampleEmployee] });
 });
-
-const port = 3310;
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
